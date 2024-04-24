cd02ec18946abac073ee2e3a22bdec51e9fcf9a8
HenningRixen <henning.rixen@freenet.de>
2024-04-24 19:13:00 +0200

    everthing except test getURL absolute

--- crawl.js
+++ crawl.js
@@ -10,9 +10,36 @@ const normalizeURL = (url) => {
     return myURL.hostname + trimmed_pathname
 }
 
+const getURLsFromHTML = (htmlBody, baseURL) => {
+    
+    const absolute_URL = []
+    // create JSDOM object, that searches automaticly in a htmlBody for links 
+    const { JSDOM } = require('jsdom')
+    const dom = new JSDOM(htmlBody)
+    // gives out arry of html links:
+    const relative_URL = dom.window.document.querySelectorAll('a')
+    
+    // incrementing absoluteURL with the baseURL added with the relativ_URL to create absolute URL
+    for (let i = 0; i < relative_URL.length; i++) {
+        if (relative_URL[i] === baseURL){
+            absolute_URL.push('/')
+        } else {
+            absolute_URL.push(baseURL + relative_URL[i])
+        }
+    
+    }
+    
+    return absolute_URL
+    
+    
+    // loop through the list and add the base url to every element
+    
+}
+
 
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML,
   }
   
--- crawl.test.js
+++ crawl.test.js
@@ -1,18 +1,53 @@
 const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
+const { getURLsFromHTML } = require('./crawl.js')
 
-test('console logs function working...', () => {
-    expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
-  });
 
-test('console logs function working...', () => {
-    expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path');
-  });
+test('normalize https /', () => {
+  expect(normalizeURL('https://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+});
 
-test('console logs function working...', () => {
-    expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
-  });
+test('normalize https', () => {
+  expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+});
 
-test('console logs function working...', () => {
-    expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path');
-  });
+test('normalize http /', () => {
+  expect(normalizeURL('http://blog.boot.dev/path/')).toBe('blog.boot.dev/path');
+});
+
+test('normalize http', () => {
+  expect(normalizeURL('https://blog.boot.dev/path')).toBe('blog.boot.dev/path');
+});
+
+test('getURl absolute', () => {
+  const htmlBody = '<a href="https://boot.dev">Learn Backend Development</a>'
+  const baseURL = 'https://boot.dev'
+  const actual = getURLsFromHTML(htmlBody, baseURL)
+  const expected = ['https://boot.dev/']
+  expect(actual).toEqual(expected)
+})
+
+
+test('getURl one', () => {
+  const htmlBody = '<a href="/path">Learn Backend Development</a>'
+  const baseURL = 'https://boot.dev'
+  const actual = getURLsFromHTML(htmlBody, baseURL)
+  const expected = ['https://boot.dev/path']
+  expect(actual).toEqual(expected)
+})
+
+test('getURl both', () => {
+  const htmlBody = '<a href="/path">Learn Backend Development</a> <a href="/path/blog">Learn Backend Development</a>' 
+  const baseURL = 'https://boot.dev'
+  const actual = getURLsFromHTML(htmlBody, baseURL)
+  const expected = ['https://boot.dev/path', 'https://boot.dev/path/blog' ]
+  expect(actual).toEqual(expected)
+})
+
+test('getURL handle error', () => {
+  const htmlBody = '<html><body><a href="path/one"><span>Boot.dev></span></a></body></html>'
+  const baseURL = 'https://blog.boot.dev'
+  const actual = getURLsFromHTML(baseURL, htmlBody)
+  const expected = [ ]
+  expect(actual).toEqual(expected)
+})
